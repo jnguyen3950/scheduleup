8ee40760a6add569488f258bbeb46050db45f8c0
Justin Nguyen <jnguyen3950@gmail.com>
2016-05-18 15:24:23 -0700

    #1 I used login form bootsnipp and put it into Landing component.

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from "./components/Layout"
+import Landing from "./components/Landing";
+import Layout from "./components/Layout";
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+var app = document.getElementById('app');
+ReactDOM.render(<Landing/>, app);
