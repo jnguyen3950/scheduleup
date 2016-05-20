f4ed5a4226667e4acd797ff720c5f3943c62f67e
Justin Nguyen <jnguyen3950@gmail.com>
2016-05-20 16:14:58 -0700

    #2 I created calender skeletal layout.

--- src/js/pages/Calender.js
+++ src/js/pages/Calender.js
@@ -3,9 +3,78 @@ import React from "react";
 export default class Calender extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Calender here.!</h1>
-      </div>
+      <div class="container">
+    		<div class="calendar-container">
+          <header>
+            <div class="col-md-offset-1">
+    				  <div class="day">18</div>
+    				  <div class="month">August</div>
+            </div>
+    			</header>
+    			<table class="calendar">
+    				<thead>
+    					<tr>
+    						<td class="col-md-2">Mon</td>
+    						<td class="col-md-2">Tue</td>
+    						<td class="col-md-2">Wed</td>
+    						<td class="col-md-2">Thu</td>
+    						<td class="col-md-2">Fri</td>
+    						<td class="col-md-2">Sat</td>
+                <td class="col-md-2">Sun</td>
+    					</tr>
+    				</thead>
+    				<tbody>
+    					<tr>
+    						<td class="prev-month">29</td>
+    						<td class="prev-month">30</td>
+    						<td class="prev-month">31</td>
+    						<td>1</td>
+    						<td>2</td>
+    						<td>3</td>
+    						<td>4</td>
+    					</tr>
+    					<tr>
+    						<td>5</td>
+    						<td>6</td>
+    						<td>7</td>
+    						<td>8</td>
+    						<td>9</td>
+    						<td>10</td>
+    						<td>11</td>
+    					</tr>
+    					<tr>
+    						<td>12</td>
+    						<td>13</td>
+    						<td>14</td>
+    						<td>15</td>
+    						<td>16</td>
+    						<td>17</td>
+    						<td class="current-day">18</td>
+    					</tr>
+    					<tr>
+    						<td>19</td>
+    						<td>20</td>
+    						<td>21</td>
+    						<td>22</td>
+    						<td>23</td>
+    						<td>24</td>
+    						<td>25</td>
+    					</tr>
+    					<tr>
+    						<td>26</td>
+    						<td>27</td>
+    						<td>28</td>
+    						<td>29</td>
+    						<td>30</td>
+    						<td>31</td>
+    						<td class="next-month">1</td>
+    					</tr>
+    				</tbody>
+    			</table>
+          <div class="ring-left"></div>
+          <div class="ring-right"></div>
+    		</div>
+    	</div>
     );
   }
 }
